6ddc45d8b033b867666e57dd5a7ef0b95f6f52e2
MantashaJahan <cupcake07638@gmail.com>
2023-10-23 13:32:47 +0600

    Verification Activity OTP Verification Activity

--- app/src/main/java/com/example/cat/OTPActivity.kt
+++ app/src/main/java/com/example/cat/OTPActivity.kt
@@ -1,11 +1,84 @@
 package com.example.cat
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.example.cat.databinding.ActivityOtpactivityBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
+import com.google.firebase.auth.PhoneAuthProvider.verifyPhoneNumber
+import java.util.concurrent.TimeUnit
 
 class OTPActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityOtpactivityBinding.inflate(layoutInflater)
+    }
+
+    var verificationId: String? = null
+    var auth: FirebaseAuth? = null
+    var dialog: ProgressDialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otpactivity)
+        setContentView(binding.root)
+
+        dialog = ProgressDialog(this@OTPActivity)
+        dialog!!.setMessage("Sending OTP....")
+        dialog!!.setCancelable(false)
+        dialog!!.show()
+
+        auth = FirebaseAuth.getInstance()
+        val phonenumber = intent.getStringExtra("phonenumber")
+        binding.phoneTxt.text = "Verify $phonenumber"
+
+        val option = PhoneAuthOptions.newBuilder(auth!!)
+            .setPhoneNumber(phonenumber!!)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this@OTPActivity)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCodeSent(
+                    verifId: String,
+                    forceResendingToken: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    super.onCodeSent(verifId, forceResendingToken)
+                    dialog!!.dismiss()
+                    verificationId = verifId
+
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+                    binding.otpView.requestFocus()
+                }
+            }).build()
+        verifyPhoneNumber(option)
+        binding.otpView.setOtpCompletionListener {
+            val credential = PhoneAuthProvider.getCredential(verificationId!!, it)
+            auth!!.signInWithCredential(credential)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        startActivity(Intent(this@OTPActivity, SetupProfileActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this@OTPActivity, "Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/cat/VarificationActivity.kt
+++ app/src/main/java/com/example/cat/VarificationActivity.kt
@@ -1,11 +1,31 @@
 package com.example.cat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cat.databinding.ActivityMainBinding
+import com.example.cat.databinding.ActivityVarificationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class VarificationActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityVarificationBinding.inflate(layoutInflater)
+    }
+    var auth: FirebaseAuth ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_varification)
+        setContentView(binding.root)
+
+
+        auth = FirebaseAuth.getInstance()
+        if(auth!!.currentUser!=null){
+            startActivity(Intent(this@VarificationActivity,MainActivity::class.java))
+            finish()
+        }
+        binding.phoneET.requestFocus()
+        binding.continueBtn.setOnClickListener{
+           var intent = intent.putExtra("phonenumber",binding.phoneET.text.toString())
+            startActivity(intent)
+        }
     }
 }
